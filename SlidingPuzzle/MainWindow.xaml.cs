b41f16c2220f0cc7e74fed5a785e945b2746105f
mattéo bertauld-bécourt <matteo.bertauld-becourt@etu.univ-smb.fr>
2024-01-11 08:42:41 +0100

    version final

--- SlidingPuzzle/MainWindow.xaml.cs
+++ SlidingPuzzle/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,9 +12,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SlidingPuzzle
 {
@@ -28,26 +32,33 @@ namespace SlidingPuzzle
 
         int[] valeurGrille;
         Label[] grille;
-        int taille = (int)Math.Pow(15,2);
+        Button[] boutons;
+        ImageBrush[] boutonSkin;
+
+        int taille = (int)Math.Pow(5,2);
+
 
 
         public MainWindow()
         {
-
             InitializeComponent();
-
             Menu FenetreMenu = new Menu();
             FenetreMenu.ShowDialog();
             if (FenetreMenu.DialogResult == false)
-                Application.Current.Shutdown();
+                System.Windows.Application.Current.Shutdown();
 
+            Outil FenetreOutil = new Outil();
+            FenetreOutil.Show();
 
             valeurGrille = new int[taille];
             grille = new Label[taille];
+            boutons = new Button[taille];
+            boutonSkin = new ImageBrush[taille];
 
             CreationGrille(taille);
             Generation_doubletableau();
-            CreerCase(taille);
+            CreerBoutons(taille);
+
             AffichageGrille();
             // configure le Timer et les événements
             // lie le timer du répartiteur à un événement appelé moteur de jeu gameengine
@@ -76,33 +87,88 @@ namespace SlidingPuzzle
             }
         }
 
-        private void CreerCase(int taille)
+        private void Clique(object sender, EventArgs e)
+        {
+            Button bouton = sender as Button;
+
+            int colonne = Grid.GetColumn(bouton);
+            int ligne = Grid.GetRow(bouton);
+            int numero = colonne * (int)Math.Sqrt(taille) + ligne;
+
+            foreach (Button bout in boutons)
+            {
+                int c2 = Grid.GetColumn(bout);
+                int l2 = Grid.GetRow(bout);
+                int num2 = c2 * (int)Math.Sqrt(taille) + l2;
+
+                if (bout.Tag == "zero")
+                {
+                    if (( (c2 == colonne - 1 || c2 == colonne + 1) && (l2 == ligne) ) || ( (l2 == ligne - 1 || l2 == ligne + 1) && (c2 == colonne) ) )
+                    {
+                        bout.Tag = "";
+                        //bout.Content = bouton.Content;
+                        bout.Visibility = Visibility.Visible;
+                        boutons[num2].Background = boutonSkin[numero];
+
+                        bouton.Visibility = Visibility.Hidden;
+                        //bouton.Content = "";
+                        bouton.Tag = "zero";
+                        boutons[numero].Background = boutonSkin[num2];
+                    }
+                }
+            }
+
+
+
+
+
+
+        }
+
+        private void CreerBoutons(int taille)
         {
             for (int i = 0; i < taille; i++)
             {
-                Label test = new Label
+                Button test2 = new Button
                 {
-                    Name = "case" + Convert.ToString(i),
-                    Content = "x",
-                    FontSize = 18,
+                    Name = "bouton" + i.ToString(),
+                    Height = 60,
+                    Width = 60,
                 };
-                test.VerticalAlignment = VerticalAlignment.Center;
-                test.HorizontalAlignment = HorizontalAlignment.Center;
-                Grid.SetRow(test, i/((int)Math.Sqrt(taille)));
-                Grid.SetColumn(test, i%((int)Math.Sqrt(taille)));
-                //Canvas.SetTop(test, 80 + i/((int)Math.Sqrt(taille))*100);
-                //Canvas.SetLeft(test, 250 + i%((int)Math.Sqrt(taille))*100);
+                test2.VerticalAlignment = VerticalAlignment.Center;
+                test2.HorizontalAlignment = HorizontalAlignment.Center;
+                Grid.SetRow(test2, i / (int)Math.Sqrt(taille));
+                Grid.SetColumn(test2, i % (int)Math.Sqrt(taille));
 
-                grille[i] = test;
-                maGrille.Children.Add(test);
+                test2.Click += Clique;
+
+                maGrille.Children.Add(test2);
+                boutons[i] = test2;
+                boutonSkin[i] = new ImageBrush();
+                boutonSkin[i].ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Images/Triopiqueur/5x5/" + i + ".jpg"));
+                boutons[i].Background = boutonSkin[i];
             }
         }
+
+
 
         private void AffichageGrille()
         {
             for(int i = 0 ; i < grille.Length ; i++)
             {
-                grille[i].Content = valeurGrille[i];
+                if (valeurGrille[i] != 0) 
+                {
+                    boutons[i].Tag = "";
+                    //boutons[i].Content = valeurGrille[i].ToString();
+                    boutons[i].Background = boutonSkin[i];
+
+                } else 
+                {
+                    boutons[i].Background = boutonSkin[i];
+                    //boutons[i].Content = "";
+                    boutons[i].Tag = "zero";
+                    boutons[i].Visibility = Visibility.Hidden;
+                }
             };
         }
 
@@ -130,7 +196,7 @@ namespace SlidingPuzzle
             }
         }
 
-        private void Generer_Click(object sender, RoutedEventArgs e)
+        public void GenererNouvelleGrille()
         {
             Generation_doubletableau();
             AffichageGrille();
@@ -138,34 +204,10 @@ namespace SlidingPuzzle
     
         private void CanvasKeyIsDown(object sender, KeyEventArgs e)
         {
-            // on gère les booléens gauche et droite en fonction de l’appui de la touche
-            if (e.Key == Key.Left)
-            {
-                goLeft = true;
-            }
-            if (e.Key == Key.Right)
-            {
-                goRight = true;
-            }
-
-            if (e.Key == Key.Space)
-            {
-                jump = true;
-                jumpPhase1 = true;
-            }
         }
 
         private void CanvasKeyIsUp(object sender, KeyEventArgs e)
         {
-            // on gère les booléens gauche et droite en fonction du relâchement de la touche
-            if (e.Key == Key.Left)
-            {
-                goLeft = false;
-            }
-            if (e.Key == Key.Right)
-            {
-                goRight = false;
-            }
         }
     }
 }
---
+++ SlidingPuzzle/Outil.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SlidingPuzzle
+{
+    /// <summary>
+    /// Logique d'interaction pour Outil.xaml
+    /// </summary>
+    public partial class Outil : Window
+    {
+        public Outil()
+        {
+            InitializeComponent();
+        }
+
+        private void Generer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
